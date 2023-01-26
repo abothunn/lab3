08fb1607a4e704d532a1db55cec2b2494a3a0910
abothunn <abothun@ucsd.edu>
2023-01-25 16:14:40 -0800

    update
    updated reverse and reverseInPlace

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,26 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+  public void testReverseInPlace321(){
+    int[] input2 = {10, 9, 8, 7, 6};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{6, 7, 8, 9, 10}, input2);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+  public void testReversed321(){
+    int[] input2 = {3, 2, 1};
+    assertArrayEquals(new int[]{1, 2, 3}, ArrayExamples.reversed(input2));
+  }
+
+  
+
 }
